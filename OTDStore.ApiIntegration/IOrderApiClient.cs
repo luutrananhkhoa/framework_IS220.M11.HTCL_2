cb0c7f6a798011ae78cddd08da5eb85423962988
Dương Nguyễn Thái <19521410@ms.uit.edu.vn>
2021-12-28 14:15:26 +0700

    Checkout cart

---
+++ OTDStore.ApiIntegration/IOrderApiClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OTDStore.ApiIntegration
+{
+    public interface IOrderApiClient
+    {
+        Task<ApiResult<bool>> CreateOrder(OrderCreateRequest register);
+    }
+}
---
+++ OTDStore.ViewModels/Sales/OrderDetailVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OTDStore.ViewModels.Sales
+{
+    public class OrderDetailVM
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
